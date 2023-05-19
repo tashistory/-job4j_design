ef604d17803bdce1c60c40655e602fea66a7c2ef
semyon <semyon.mihalich@gmail.com>
2023-05-19 12:14:25 +0500

    7. ListIterator [#350217 #417086]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,56 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (iterator.nextIndex() == index) {
+                iterator.add(value);
+                break;
+            }
+            iterator.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        addBefore(list, index + 1, value);
+       /* Objects.checkIndex(index, list.size());
+       ListIterator<T> iterator = list.listIterator(index + 1);
+       iterator.add(value);
+        iterator.next(); */
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.remove();
+            }
+        }
+
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> iterator = list.listIterator();
+        while (iterator.hasNext()) {
+            if (filter.test(iterator.next())) {
+                iterator.set(value);
+            }
+        }
+
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> eIterator = elements.listIterator();
+        while (eIterator.hasNext()) {
+            T tmp = eIterator.next();
+            removeIf(list, el -> el.equals(tmp));
+        }
+    }
+
+}
---
+++ src/test/java/ru/job4j/iterator/ListUtilsTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.iterator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+class ListUtilsTest {
+
+    private List<Integer> input;
+
+    @BeforeEach
+    void setUp() {
+        input = new ArrayList<>(Arrays.asList(1, 3));
+    }
+
+    @Test
+    void whenAddBefore() {
+        ListUtils.addBefore(input, 1, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenAddBeforeWithInvalidIndex() {
+        assertThatThrownBy(() -> ListUtils.addBefore(input, 3, 2))
+                .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void whenAddAfter() {
+        ListUtils.addAfter(input, 0, 2);
+        assertThat(input).hasSize(3).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    void whenRemoveIf() {
+        ListUtils.removeIf(input, el -> el.equals(3));
+        assertThat(input).hasSize(1).containsSequence(1);
+    }
+    @Test
+    void wrenReplaceIf() {
+        ListUtils.replaceIf(input, el -> el.equals(3), 4);
+        assertThat(input).hasSize(2).containsSequence(1, 4);
+    }
+    @Test
+    void wrenRemoveAll() {
+        ListUtils.addBefore(input, 1, 2);
+        ListUtils.addBefore(input, 2, 4);
+        ListUtils.addBefore(input, 2, 5);
+        ListUtils.removeAll(input, new ArrayList<>(Arrays.asList(2, 4, 5)));
+        assertThat(input).hasSize(2).containsSequence(1, 3);
+    }
+}
