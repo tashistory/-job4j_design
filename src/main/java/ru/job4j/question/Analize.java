7e2149939cf39fddc2f89f7cd4369f81380442a5
semyon <semyon.mihalich@gmail.com>
2023-05-30 13:36:42 +0500

    3. Статистика по коллекции.

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -4,17 +4,26 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Analize {
-    static long added;
-    static long changed;
+    static int added = 0;
+    static int changed = 0;
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Map<Integer, User> map = previous.stream()
                 .collect(Collectors.toMap(User::getId, user -> user,
                         (existing, replacement) -> existing));
 
-       added = current.stream().filter(user -> !map.containsKey(user.getId())).count();
-       changed = current.stream().filter(user -> !map.containsValue(user) && map.containsKey(user.getId())).count();
-       current.forEach(user -> map.remove(user.getId()));
-        return new Info((int) added, (int) changed, map.size());
+        current.forEach(user -> {
+            if (!map.containsKey(user.getId())) {
+                added++;
+            }
+            if (!map.containsValue(user) && map.containsKey(user.getId())) {
+                changed++;
+            }
+            map.remove(user.getId());
+        });
+        Info rsl = new Info(added, changed, map.size());
+        added = 0;
+        changed = 0;
+        return rsl;
     }
 }
