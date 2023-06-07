3f2d37ea65661833fed59dea8010bdd20a33417b
semyon <semyon.mihalich@gmail.com>
2023-06-07 10:57:38 +0500

    3. Классы вывода PrintStream / PrintWriter [#504999]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
@@ -15,7 +16,7 @@ public class EvenNumberFile {
                 boolean rslt = Integer.parseInt(line) % 2 == 0;
                 System.out.println(line + " - " + rslt);
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
