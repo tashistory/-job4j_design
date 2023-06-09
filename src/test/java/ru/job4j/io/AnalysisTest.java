2b49da6b98848ba00954b547937e86c09525f200
semyon <semyon.mihalich@gmail.com>
2023-06-09 17:07:53 +0500

    8. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalysisTest.java
+++ src/test/java/ru/job4j/io/AnalysisTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.io.TempDir;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -13,23 +15,22 @@ class AnalysisTest {
     void drop(@TempDir Path tempDir) throws IOException {
         File source = tempDir.resolve("source.txt").toFile();
         try (PrintWriter out = new PrintWriter(source)) {
-            out.println("""
-                    200 10:56:01
-                    500 10:57:01
-                    400 10:58:01
-                    300 10:59:01
-                    500 11:01:02
-                    200 11:02:02""");
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("300 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
         }
         File target = tempDir.resolve("target.txt").toFile();
         Analysis analysis = new Analysis();
         analysis.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-
-        StringBuilder rsl = new StringBuilder();
+        List<String> actual = List.of("10:57:01;10:59:01;", "11:01:02;11:02:02;");
+        List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(rsl::append);
+            in.lines().forEach(rsl::add);
         }
-        assertThat("10:57:01;10:59:01;11:01:02;11:02:02;").hasToString(rsl.toString());
+        assertThat(actual).isEqualTo(rsl);
     }
 
 }
