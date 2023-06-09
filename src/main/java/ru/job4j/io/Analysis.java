29eaab71355ce55d2fa06d4de356a59b1ebaa7c2
semyon <semyon.mihalich@gmail.com>
2023-06-09 14:12:50 +0500

    7. Анализ доступности сервера [#859]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -6,15 +6,18 @@ public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
              PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
-            boolean k = false;
+            boolean flag = false;
             for (String line = read.readLine(); line != null; line = read.readLine()) {
-                if ((line.startsWith("400") || line.startsWith("500")) && !k) {
-                    out.printf("%s;", line.split(" ")[1]);
-                    k = true;
-                }
-                if ((line.startsWith("200") || line.startsWith("300")) && k) {
-                    out.printf("%s;%s", line.split(" ")[1], System.lineSeparator());
-                    k = false;
+                if (line.startsWith("400") || line.startsWith("500")) {
+                    if (!flag) {
+                        out.printf("%s;", line.split(" ")[1]);
+                        flag = true;
+                    }
+                } else {
+                    if (flag) {
+                        out.printf("%s;%s", line.split(" ")[1], System.lineSeparator());
+                        flag = false;
+                    }
                 }
             }
         } catch (IOException e) {
