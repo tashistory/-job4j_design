e36b0fb4fa08c795d012e1da19a17d675cfcb270
semyon <semyon.mihalich@gmail.com>
2023-06-15 15:36:36 +0500

    14. Архивировать проект [#861 #423076]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,27 +10,17 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) throws IOException {
-        ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(target));
-        for (Path source : sources) {
-            zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
-            Files.copy(source, zip);
-        }
-        zip.close();
-    }
-
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(target))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                Files.copy(source, zip);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws IOException {
+    public void getZip(String[] args)throws IOException {
         ArgsName argumets = ArgsName.of(args);
         String directory = argumets.get("d");
         String exclude = argumets.get("e");
@@ -38,8 +28,11 @@ public class Zip {
         Search.validation(new String[]{directory, exclude});
         Path start = Paths.get(directory);
         List<Path> path = Search.search(start, p -> !p.toFile().getName().endsWith(exclude));
-        path.forEach(System.out::println);
+        packFiles(path, new File(output));
+    }
+
+    public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
-        zip.packFiles(path, new File(output));
+        zip.getZip(args);
     }
 }
