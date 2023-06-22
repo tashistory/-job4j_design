f5afd7d5305b6ec5748f8657265ec17ca6414850
semyon <semyon.mihalich@gmail.com>
2023-06-22 14:26:15 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,6 +1,5 @@
 package ru.job4j.io;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,10 +8,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        char sex = 'М';
+        float height = 175.4f;
+        short schoolNumber = 1234;
+        byte houseNumber = 54;
+        boolean married = true;
+        double amountAccount = 3445454565465654575676788.44D;
+        long numberLikes = 24245554465L;
+        LOG.debug("User info name : {}, age : {}, sex : {}, height : {}, schoolNumber : {}, "
+                        + "houseNumber : {}, married : {}, amountAccount : {}, numberLikes : {}",
+                name, age, sex, height, schoolNumber, houseNumber, married, amountAccount, numberLikes);
     }
 }
