cbdda9a522c97c3e35f999134d314f13a89a3218
semyon <semyon.mihalich@gmail.com>
2023-06-27 13:51:08 +0500

    парсинг сайта

--- src/main/java/ru/job4j/test/Main.java
+++ src/main/java/ru/job4j/test/Main.java
@@ -4,13 +4,16 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
       System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        WebDriver webDriver = new ChromeDriver(options);
         webDriver.get("https://ru.chabad.org/calendar/candlelighting_cdo/locationid/681/locationtype/1");
         System.out.println(webDriver.getPageSource());
         webDriver.quit();
