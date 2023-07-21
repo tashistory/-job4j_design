7ca85c40cd5550b50f32dc0544fe77587e6aa6a5
semyon <semyon.mihalich@gmail.com>
2023-07-21 16:55:45 +0500

    0. JDBC

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,24 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config config = new Config("data/app.properties");
+        config.load();
+        Class.forName(config.value("hibernate.connection.driver_class"));
+        try (Connection connection = DriverManager.getConnection(
+                config.value("hibernate.connection.url"),
+                config.value("hibernate.connection.username"),
+                config.value("hibernate.connection.password"))) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
