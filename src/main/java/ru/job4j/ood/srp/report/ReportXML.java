a3dc55ab30066879865a2b5512993213ed4602ff
semyon <semyon.mihalich@gmail.com>
2023-10-16 17:16:52 +0500

    1. Поддержка форматов XML, JSON в генераторе отчетов. [#851 #451534]

--- src/main/java/ru/job4j/ood/srp/report/ReportXML.java
+++ src/main/java/ru/job4j/ood/srp/report/ReportXML.java
@@ -1,15 +1,14 @@
 package ru.job4j.ood.srp.report;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
 import ru.job4j.ood.srp.model.Employee;
-import ru.job4j.ood.srp.store.MemStore;
 import ru.job4j.ood.srp.store.Store;
-
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.function.Predicate;
@@ -17,39 +16,90 @@ import java.util.function.Predicate;
 
 public class ReportXML implements Report {
     private final Store store;
+    private final DateTimeParser<Calendar> dateTimeParser;
     private final JAXBContext context;
 
-    public ReportXML(Store store, JAXBContext context) {
+    public ReportXML(Store store, DateTimeParser<Calendar> dateTimeParser, JAXBContext context) {
         this.store = store;
+        this.dateTimeParser = dateTimeParser;
         this.context = context;
     }
 
     @XmlRootElement(name = "employees")
     public static class Employees {
 
-        private List<Employee> employee;
+        private List<XMLEmployee> employee;
 
         public Employees() {
 
         }
 
-        public Employees(List<Employee> employee) {
+        public Employees(List<XMLEmployee> employee) {
             this.employee = employee;
         }
 
-        public List<Employee> getEmployee() {
+        public List<XMLEmployee> getEmployee() {
             return employee;
         }
 
-        public void setEmployee(List<Employee> employee) {
+        public void setEmployee(List<XMLEmployee> employee) {
             this.employee = employee;
+        }
+    }
+
+    public static class XMLEmployee {
+        private String name;
+        private String hired;
+        private String fired;
+        private double salary;
+
+        public XMLEmployee(String name, String hired, String fired, double salary) {
+            this.name = name;
+            this.hired = hired;
+            this.fired = fired;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getHired() {
+            return hired;
+        }
+
+        public void setHired(String hired) {
+            this.hired = hired;
+        }
+
+        public String getFired() {
+            return fired;
+        }
+
+        public void setFired(String fired) {
+            this.fired = fired;
+        }
+
+        public double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(double salary) {
+            this.salary = salary;
         }
     }
 
     @Override
     public String generate(Predicate<Employee> filter) throws JAXBException {
-
-        Employees employees = new Employees(store.findBy(filter));
+        List<XMLEmployee> xmlEmployees = new ArrayList<>();
+        for (Employee emp : store.findBy(filter)) {
+            xmlEmployees.add(new XMLEmployee(emp.getName(), dateTimeParser.parse(emp.getHired()), dateTimeParser.parse(emp.getFired()), emp.getSalary()));
+        }
+        Employees employees = new Employees(xmlEmployees);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
         String xml = "";
@@ -61,23 +111,5 @@ public class ReportXML implements Report {
             e.printStackTrace();
         }
         return xml;
-    }
-
-
-    public static void main(String[] args) throws JAXBException {
-        MemStore store = new MemStore();
-        Calendar now = Calendar.getInstance();
-        Employee worker = new Employee("Ivan", now, now, 100);
-        Employee worker2 = new Employee("Stepan", now, now, 90);
-        Employee worker3 = new Employee("Alex", now, now, 120);
-        store.add(worker);
-        store.add(worker2);
-        store.add(worker3);
-        Report engine = new ReportXML(store, JAXBContext.newInstance(ReportXML.Employees.class));
-        System.out.println(engine.generate(em -> true));
-
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-        String formattedDate = dateFormat.format(now.getTime());
-        System.out.println(formattedDate);
     }
 }
--- src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
@@ -1,13 +1,14 @@
 package ru.job4j.ood.srp.report;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
 import ru.job4j.ood.srp.model.Employee;
 import ru.job4j.ood.srp.store.MemStore;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static org.assertj.core.api.Assertions.*;
@@ -18,20 +19,25 @@ class ReportXMLTest {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
+        Employee worker2 = new Employee("Stepan", now, now, 90);
+        Employee worker3 = new Employee("Alex", now, now, 120);
         store.add(worker);
-        Report engine = new ReportXML(store, JAXBContext.newInstance(ReportXML.Employees.class));
-        String ls = System.lineSeparator();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        store.add(worker2);
+        store.add(worker3);
+        DateTimeParser<Calendar> dateTimeParser = new ReportDateTimeParser();
+        Report engine = new ReportXML(store, dateTimeParser, JAXBContext.newInstance(ReportXML.Employees.class));
         StringBuilder expect = new StringBuilder()
                 .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
-                .append("<employees>")
-                .append("<employee>")
-                .append("<fired>").append(dateFormat.format(worker.getFired().getTime())).append("</fired>")
-                .append("<hired>").append(dateFormat.format(worker.getHired().getTime())).append("</hired>")
-                .append("<name>").append(worker.getName()).append("</name>")
-                .append("<salary>").append(worker.getSalary()).append("</salary>")
-                .append("</employee>")
-                .append("</employees>");
+                .append("<employees>");
+        for (Employee em : store.findBy(sr -> true)){
+            expect.append("<employee>")
+                    .append("<fired>").append(dateTimeParser.parse(em.getFired())).append("</fired>")
+                    .append("<hired>").append(dateTimeParser.parse(em.getHired())).append("</hired>")
+                    .append("<name>").append(em.getName()).append("</name>")
+                    .append("<salary>").append(em.getSalary()).append("</salary>")
+                    .append("</employee>");
+        }
+        expect.append("</employees>");
         String otv = engine.generate(em -> true);
         assertThat(otv.replaceAll(" ", "").replaceAll("\n", "")).isEqualTo(expect.toString().replaceAll(" ", ""));
     }
